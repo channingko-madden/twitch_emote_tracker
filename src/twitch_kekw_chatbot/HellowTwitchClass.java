58c0236fec58bea57fb8c7d74b65e58bf45bc409
Channing Ko-Madden <channing.ko-madden@survice.com>
2020-12-17 13:26:43 -0500

    test pushing eclipse proj

---
+++ src/twitch_kekw_chatbot/HellowTwitchClass.java
@@ -0,0 +1,11 @@
+package twitch_kekw_chatbot;
+
+public class HellowTwitchClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow Twitch");
+
+	}
+
+}
